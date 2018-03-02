9905109ee3685d442d82a22226ff4959aeb70226
Taher6168 <36978289+Taher6168@users.noreply.github.com>
2018-03-02 09:02:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'CCKYDenI3wbDPt3cWO3C4ZvP9',
+  consumer_secret:      'rL5lYrHmZ1Zk5XUYxSv13WIgRuAxX3cuaX8Ty6SHsOENpZjNuP',
+  access_token:         '946060876632637440-A20mnn920Awlo4d8BCWC2QRsMg6AeSM',
+  access_token_secret:  'gpV3NszQBFsXJ29AeBkYhP5K3zVcuPkPU88kPz85lhTwj'
+}
+
+module.exports = config;
